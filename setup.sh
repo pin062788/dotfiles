19fdc029f158e06a77272923fc95aa281461ecd3
Wang lei <wang.lei@rea-group.com>
2013-11-01 19:28:09 +0800

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,33 +1,29 @@
 ln_it_to_home()
 {
-    target_name="$1"
-    
+    source_file="$1"
+    target_file="$2"
 
-    if [ -z "$1" ]; then
-        return 1
+    rm -rf "$HOME/${target_file}.bak"
+
+    if [ -e "$HOME/${target_file}" ]; then
+        cp -rf "$HOME/${target_file}" "$HOME/${target_file}.bak"
     fi  
 
+    ln -sf -n -v `pwd -P`/"$source_file" $HOME/"$target_file"
 
-    rm -rf "$HOME/.${target_name}.bak"
-
-    cp -rf "$HOME/.${target_name}" "$HOME/.${target_name}.bak"
-
-    ln -sf -n -v `pwd -P`/"$target_name" $HOME/."$target_name"
-
-    return $?
+    #return $?
 }
 
+set -x
 
+ln_it_to_home "bash/bashrc" ".bashrc"
+ln_it_to_home "bash/bashrc_profile" ".bashrc_profile"
+ln_it_to_home "bash/bash_alias" ".bash_alias"
 
-ln_it_to_home "bash/bashrc"
-ln_it_to_home "bash/bashrc_profile"
-ln_it_to_home "bash/bash_alias"
+ln_it_to_home "vim" "vim"
+ln_it_to_home "vim/vimrc" ".vimrc"
 
-ln_it_to_home "vim"
-ln_it_to_home "vim/vimrc"
-
-ln_it_to_home "gitconfig"
+ln_it_to_home "gitconfig" ".gitconfig"
 ln_it_to_home "gitignore"
 ln_it_to_home "git-completion.bash"
 ln_it_to_home "githelpers"
-
