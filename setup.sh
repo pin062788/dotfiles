4e1240cf854f159e7831a8448d29227849f75232
Wang lei <wang.lei@rea-group.com>
2013-11-01 18:33:22 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,33 @@
-rm -rf ~/.bashrc.bak
-rm -rf ~/.bash_aliases.bak
-rm -rf ~/.bash_profile.bak
-rm -rf ~/.vim.bak
-rm -rf ~/.vimrc.bak
- 
-mv ~/.bashrc ~/.bashrc.bak
-mv ~/.bash_aliases ~/.bash_aliases.bak
-mv ~/.bash_profile ~/.bash_profile.bak
-mv ~/.vim ~/.vim.bak
-mv ~/.vimrc ~/.vimrc.bak
+ln_it_to_home()
+{
+    target_name="$1"
+    
 
-ln -s ./dotfiles/bash/bashrc ~/.bashrc
-ln -s ./dotfiles/bash/bash_aliases ~/.bash_aliases
-ln -s ./dotfiles/bash/bash_profile ~/.bash_profile
-ln -s ./dotfiles/vim/ ~/.vim
-ln -s ./dotfiles/vim/vimrc ~/.vimrc
+    if [ -z "$1" ]; then
+        return 1
+    fi  
+
+
+    rm -rf "$HOME/.${target_name}.bak"
+
+    cp -rf "$HOME/.${target_name}" "$HOME/.${target_name}.bak"
+
+    ln -sf -n -v `pwd -P`/"$target_name" $HOME/."$target_name"
+
+    return $?
+}
+
+
+
+ln_it_to_home "bash/bashrc"
+ln_it_to_home "bash/bashrc_profile"
+ln_it_to_home "bash/bash_alias"
+
+ln_it_to_home "vim"
+ln_it_to_home "vim/vimrc"
+
+ln_it_to_home "gitconfig"
+ln_it_to_home "gitignore"
+ln_it_to_home "git-completion.bash"
+ln_it_to_home "githelpers"
+
